d9e1505d8b07441c3f8ff131d3d66be527c09428
FaisalRasheed1174 <faisalrasheed1174@gmail.com>
2022-09-10 18:52:14 +0500

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express()
 const port=process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send("<h2>Faisal Devos</h2>")
+    res.send("<h2>Faisal Devops</h2>")
 })
 
 app.listen(port,()=>console.log(`listneing on port ${port}`))
